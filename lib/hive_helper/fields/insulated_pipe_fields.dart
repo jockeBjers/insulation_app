2e637522f69e289d29bddca98893aa84d2615ed7
JockeBjers <joakim.bjerselius@hotmail.com>
2025-04-11 21:43:26 +0200

    remove hive db generated files

--- lib/hive_helper/fields/insulated_pipe_fields.dart
+++
@@ -1,6 +0,0 @@
-class InsulatedPipeFields {
-  static const int size = 0;
-  static const int length = 1;
-  static const int firstLayerMaterial = 2;
-  static const int secondLayerMaterial = 3;
-}
--- lib/hive_helper/fields/pipe_size_fields.dart
+++
@@ -1,4 +0,0 @@
-class PipeSizeFields {
-	static const int label = 0;
-	static const int diameter = 1;
-}
